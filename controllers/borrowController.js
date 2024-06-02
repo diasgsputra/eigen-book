d78cce52c726794e7ed332bcab064013e7a7e367
DS23666X <diasgsputra@gmail.com>
2024-06-02 14:14:58 +0700

    add condition borrowing book

--- controllers/borrowController.js
+++ controllers/borrowController.js
@@ -4,6 +4,21 @@ const addBorrowBook = async (req, res) => {
     const { id_book, id_member} = req.body;
   
     try {
+        const [result] = await sequelize.query(
+            "SELECT COUNT(br.id) AS count "+
+            "FROM borrows br "+
+            "INNER JOIN members m ON m.id = br.id_member "+
+            "WHERE m.id = :id AND m.is_penalty = 0",
+            {
+              type: sequelize.QueryTypes.query,
+              replacements: { id: id_member }
+            }
+          );
+          const count = result[0].count;
+          if(count > 1){
+            return res.status(200).json({ message: "Member has borrow more than 1 book" });
+          }
+
       let borrow;
         // Create new borrow book
         borrow = await Borrow.create({
--- routes/borrow.js
+++ routes/borrow.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const { addBorrowBook } = require("../controllers/borrowController");
 
 const path = "/api/borrow"
-router.get(path + "/add", (req, res) => addBorrowBook(req, res));
+router.post(path + "/add", (req, res) => addBorrowBook(req, res));
 
 module.exports = router;
