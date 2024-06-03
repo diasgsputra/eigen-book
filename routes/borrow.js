6c432ccbdccb4541f7c3d1196cf7b8cfdafe1863
DS23666X <diasgsputra@gmail.com>
2024-06-03 14:17:12 +0700

    swagger return book

--- routes/borrow.js
+++ routes/borrow.js
@@ -31,6 +31,25 @@ const { addBorrowBook, returnBook } = require("../controllers/borrowController")
  *         description: Save successful
  *       500:
  *         description: Internal Server Error
+ *  
+ * /api/borrow/return/{id_borrow}:
+ *   get:
+ *     summary: Return book
+ *     tags: [Borrow-book]
+ *     parameters:
+ *       - in: path
+ *         name: id_borrow
+ *         required: true
+ *         description: id borrow
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: Successful
+ *       404:
+ *         description: Not found
+ *       500:
+ *         description: Internal Server Error
  */
 const path = "/api/borrow"
 router.post(path + "/add", (req, res) => addBorrowBook(req, res));
